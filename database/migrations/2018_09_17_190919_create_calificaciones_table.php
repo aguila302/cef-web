43db703eee3e25f691472aa51a4c0e90c5d209e6
Alfonso Hernández Montoya <ahernandezmo@calymayor.com.mx>
2018-09-17 17:15:16 -0500

    ok

---
+++ database/migrations/2018_09_17_190919_create_calificaciones_table.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCalificacionesTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('calificaciones', function (Blueprint $table) {
+			$table->increments('id');
+			$table->unsignedInteger('autopista_id');
+			$table->unsignedInteger('cuerpo_id');
+			$table->unsignedInteger('seccion_id');
+			$table->unsignedInteger('elemento_id');
+			$table->unsignedInteger('defecto_id');
+			$table->unsignedInteger('intensidad_id');
+			$table->unsignedInteger('calificacion');
+
+			$table->foreign('autopista_id')
+				->references('id')->on('autopistas')
+				->onDelete('cascade');
+
+			$table->foreign('cuerpo_id')
+				->references('id')->on('cuerpos')
+				->onDelete('cascade');
+
+			$table->foreign('seccion_id')
+				->references('id')->on('secciones')
+				->onDelete('cascade');
+
+			$table->foreign('elemento_id')
+				->references('id')->on('elementos')
+				->onDelete('cascade');
+
+			$table->foreign('defecto_id')
+				->references('id')->on('defectos')
+				->onDelete('cascade');
+
+			$table->foreign('intensidad_id')
+				->references('id')->on('intensidades')
+				->onDelete('cascade');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists('calificaciones');
+	}
+}
