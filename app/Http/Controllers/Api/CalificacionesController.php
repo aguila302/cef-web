ef56ab4c9838e24a31ae1a2e70e519112f2fe983
hernandez_pon <hernandez_pon@live.com.mx>
2018-09-19 09:31:45 -0500

    ok

--- app/Http/Controllers/Api/CalificacionesController.php
+++ app/Http/Controllers/Api/CalificacionesController.php
@@ -8,26 +8,6 @@ use Illuminate\Http\Request;
 class CalificacionesController extends ApiController
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Registrar una calificación en el origen de datos.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -54,50 +34,5 @@ class CalificacionesController extends ApiController
                 'id'         => $calificacion->id,
                 'created_at' => $calificacion->created_at->toDateTimeString(),
             ], 201);
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
